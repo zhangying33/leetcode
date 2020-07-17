c29044eb397d8e326885ec453a55d1f5d0339409
smily <zhyinghappy@126.com>
2020-07-17 23:36:53 +0800

    dd

---
+++ php/zhangying/7.17zhangying.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution
+{
+
+    /**
+     * @param ListNode $l1
+     * @param ListNode $l2
+     * @return ListNode
+     */
+    function mergeTwoLists($l1, $l2)
+    {
+        //返回当前两个链表合并之后的头节点(每一层都返回排序好的链表头)
+        if ($l1 === null) return $l2;
+        if ($l2 === null) return $l1;
+
+        if ($l1->val < $l2->val) {
+            $l1->next = $this->mergeTwoLists($l1->next, $l2);
+            return $l1;
+        } else {
+            $l2->next = $this->mergeTwoLists($l1, $l2->next);
+            return $l2;
+        }
+    }
+}
