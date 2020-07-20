ac04feadf55f61deed5939a48fe7860100236dcf
smily <zhyinghappy@126.com>
2020-07-20 23:24:14 +0800

    7.21

---
+++ php/zhangying/7.20zhangying.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $m
+     * @param Integer $n
+     * @return ListNode
+     */
+    function reverseBetween($head, $m, $n) {
+        $dummy = new ListNode(0);
+        $dummy->next = $head;
+        $pre = $dummy;
+        for($i=1;$i<$m;$i++){
+            $pre = $pre->next;
+        }
+        $head = $pre->next;
+        for($i=$m;$i<$n;$i++){
+            $nex = $head->next;
+            $head->next = $nex->next;
+            $nex->next = $pre->next;
+            $pre->next = $nex;
+        }
+        return $dummy->next;
+    }
+}
